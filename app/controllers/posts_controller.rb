57ae0f7fc7f4d0f44e8c51a5da10b14506ea5d4c
jbsacks <josh.sacks@kellogg.northwestern.edu>
2022-05-18 14:42:18 +0000

    main

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
     @post["posted_on"] = params["post"]["posted_on"]
     @post["place_id"] = params["post"]["place_id"]
     @post.save
-    redirect_to "/places/#@post["place_id"]}"
+    redirect_to "/posts"
   end 
   
   end
