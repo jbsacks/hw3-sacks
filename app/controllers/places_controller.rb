cbaff229d2976cf64e5c715d52ff4a429beda7d1
jbsacks <josh.sacks@kellogg.northwestern.edu>
2022-05-18 13:32:35 +0000

    3rd

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,19 @@
+def show 
+    @place = Place.find_by ({"id" => params["id"]})
+    @posts = Post.where({place_id} => @place["id"])
+end 
+
+def new 
+    @place = Place.new
+end 
+
+def create 
+    @place = Place.new
+    @place = ["name"] = params["place"]["name"]
+    @place.save
+    redirect_to "/places"
+end 
+
+
+class PlacesController < ApplicationController
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+
+    def index
+      @posts = Post.all
+    end
+  
+  
+    
+  def new 
+    @post = Post.new
+    @post["place_id"] = params["place_id"]
+    @place = Place.find_by({"id" => params["place_id"]})
+  end 
+  
+  def create 
+    @post = Post.new
+    @post["title"] = params["post"]["title"]
+    @post["description"] = params["post"]["description"]
+    @post["posted_on"] = params["post"]["posted_on"]
+    @post["place_id"] = params["post"]["place_id"]
+    @post.save
+    redirect_to "/places/#@post["place_id"]}"
+  end 
+  
+  end
+  
